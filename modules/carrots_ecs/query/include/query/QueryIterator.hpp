82066458cb015b5f5eb865931f98ac48f0c4875e
tigerplush <christopher.putz@corgibase.de>
2025-12-21 15:26:43 +0100

    Finally a working version of Query

--- modules/carrots_ecs/query/include/query/QueryIterator.hpp
+++ modules/carrots_ecs/query/include/query/QueryIterator.hpp
@@ -9,41 +9,79 @@ namespace CarrotsEcs
 {
     namespace Query
     {
+        using ComponentId = CarrotsEcs::Component::ComponentId;
+        using Entity = CarrotsEcs::Entity::Entity;
         using TableId = CarrotsEcs::Table::TableId;
         using TableRow = CarrotsEcs::Table::TableRow;
-        template<typename... OutputType>
-        class QueryIterator : public CarrotsStd::Iterator::Iterator<std::tuple<OutputType...>>
+        template <typename... Components>
+        class QueryIterator : public CarrotsStd::Iterator::Iterator<std::tuple<Components...>>
         {
         public:
             QueryIterator(std::unique_ptr<IQuery> t_query)
-            : m_query(std::move(t_query))
-            , m_current_table_id(0)
-            , m_current_row(0)
+                : m_query(std::move(t_query)), m_current_table_id(0), m_current_row(0)
             {
-
             }
+
         public:
-            CarrotsStd::Option::Option<std::tuple<OutputType...>> next() override
+            CarrotsStd::Option::Option<std::tuple<Components...>> next() override
             {
-                if(m_current_row > m_query->row_count(m_current_table_id))
+                if (m_current_row >= m_query->row_count(m_current_table_id))
                 {
                     m_current_table_id += 1;
                     m_current_row = 0;
                 }
-                if(m_current_table_id > m_query->table_count())
+                if (m_current_table_id >= m_query->table_count())
                 {
                     return CarrotsStd::Option::None;
                 }
-                // OutputType temp = m_world->build()?;
-                // m_current_row += 1;
-                // return Option(temp);
-                return CarrotsStd::Option::None;
+                std::tuple<Components...> current_tuple = build_tuple();
+                m_current_row += 1;
+                return CarrotsStd::Option::Some(std::move(current_tuple));
             }
-        private:
-            void build_tuple()
-            {
 
+        private:
+            std::tuple<Components...> build_tuple()
+            {
+                // for every component:
+                // fetch component from current table and id
+                // pack then in tuple
+                std::tuple<Components...> current_tuple = {fetch_entity_or_component<Components>()...};
+                return current_tuple;
             }
+            template <typename EntityOrComponent>
+            EntityOrComponent fetch_entity_or_component()
+            {
+                using BaseType = std::remove_cvref_t<EntityOrComponent>;
+
+                if constexpr (std::is_same_v<BaseType, Entity>)
+                {
+                    return fetch_entity<EntityOrComponent>();
+                }
+                else
+                {
+                    return fetch_component<EntityOrComponent>();
+                }
+            }
+
+            template <typename Component>
+            Component fetch_component()
+            {
+                ComponentId component_id = ComponentId::from<std::remove_cvref_t<Component>>();
+                void *component_ptr = m_query
+                    ->get_table(m_current_table_id)
+                    .get_column(component_id)
+                    ->at(m_current_row);
+                using BaseType = std::remove_reference_t<Component>;
+                return *reinterpret_cast<BaseType *>(component_ptr);
+            }
+            template <typename EntityType>
+            EntityType fetch_entity()
+            {
+                return m_query
+                    ->get_table(m_current_table_id)
+                    .get_entity(m_current_row);
+            }
+
         private:
             std::unique_ptr<IQuery> m_query;
             TableId m_current_table_id;
