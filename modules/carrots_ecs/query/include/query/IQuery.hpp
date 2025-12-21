4fd3dddc5ab4526438afba589fc23cddc103822a
tigerplush <christopher.putz@corgibase.de>
2025-12-21 14:29:00 +0100

    Added type erased IQuery interface so that QueryIterator can ask IQuery for info

---
+++ modules/carrots_ecs/query/include/query/IQuery.hpp
@@ -0,0 +1,20 @@
+#ifndef CARROTS_ECS_QUERY_I_QUERY_HPP_
+#define CARROTS_ECS_QUERY_I_QUERY_HPP_
+
+namespace CarrotsEcs
+{
+    namespace Query
+    {
+        using TableId = CarrotsEcs::Table::TableId;
+        /// Type erased interface for queries so the iterator can access
+        /// whatever it needs to access.
+        class IQuery
+        {
+        public:
+            virtual const size_t table_count() const = 0;
+            virtual const size_t row_count(TableId table_id) const = 0;
+        };
+    } // namespace Query
+} // namespace CarrotsEcs
+
+#endif
