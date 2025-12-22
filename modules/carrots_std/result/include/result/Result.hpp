ec39f821829d655286406fb21cd3096148da51f1
tigerplush <christopher.putz@corgibase.de>
2025-12-22 18:29:19 +0100

    prepared results crate

---
+++ modules/carrots_std/result/include/result/Result.hpp
@@ -0,0 +1,12 @@
+#ifndef CARROTS_STD_RESULT_RESULT_HPP_
+#define CARROTS_STD_RESULT_RESULT_HPP_
+
+namespace CarrotsStd
+{
+    namespace Result
+    {
+
+    } // namespace Result
+} // namespace CarrotsStd
+
+#endif
---
+++ modules/carrots_std/result/tests/ResultTests.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include "result/Result.hpp"
+
+using namespace CarrotsStd::Result;
+
+TEST(ResultTest, Result)
+{
+}
